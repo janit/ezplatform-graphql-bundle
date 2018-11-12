14ffad46de8224f6895ce22f97ab23096d17d293
Jani Tarvainen <janit@iki.fi>
2018-11-12 13:45:11 +0200

    Set child limit to 1000

--- GraphQL/Resolver/LocationResolver.php
+++ GraphQL/Resolver/LocationResolver.php
@@ -50,7 +50,7 @@ class LocationResolver
     {
         if (isset($args['id'])) {
             return $this->locationService->loadLocationChildren(
-                $this->locationService->loadLocation($args['id']),0,100
+                $this->locationService->loadLocation($args['id']),0,1000
             )->locations;
         }
     }
